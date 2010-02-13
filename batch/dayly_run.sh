d7baee7ded5d3774de24a2053c90c2aa8d16b6a1
Yoshikuni Katayama <katayama@l-house.net>
2010-02-13 14:59:26 +0900

    バッチファイルをツリーから削除

--- batch/dayly_run.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$PATH:/usr/local/pgsql/bin:/usr/local/pgsql/lib
-export LD_LIBRARY_PATH=/usr/local/pgsql/lib
-
-cd /app/gw/rails/enote
-
-/usr/local/bin/ruby /app/gw/rails/enote/send_reminder_mail.rb
